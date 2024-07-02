c97f9068cd39d1bb1a11a9a650261bba2ab91726
joao0paulo0 <joao.pteixeira2005@gmail.com>
2024-07-02 16:30:41 +0100

    mongo add

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
 const port = 3000;
+
+const mongoURI = 'mongodb://localhost:27017/pw_library';
+
+// Conectando ao MongoDB com Mongoose
+mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((err) => {
+    console.error('Error MongoDB', err);
+  });
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
